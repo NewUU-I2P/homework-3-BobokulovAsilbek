d2b02b473781ddfccffa00b4cc957aa5ac3b9059
BobokulovAsilbek <147586326+BobokulovAsilbek@users.noreply.github.com>
2023-11-09 18:00:25 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,5 +1,4 @@
-#include <cmath>
-float problemSolution1(float consumed_water) {
+void problemSolution1(float consumed_water) {
     float cost;
     // write your code here
     if(consumed_water <= 0) return float(0);
@@ -9,5 +8,5 @@ float problemSolution1(float consumed_water) {
     if(consumed_water > 50) cost += min(consumed_water - 50, float(10)) * 1.4;
     if(consumed_water > 60) cost += (consumed_water - 60) * 1.5;
     
-    return cost;
+    cout << cost;
 }
