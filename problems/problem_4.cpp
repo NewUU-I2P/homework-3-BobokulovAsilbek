09b3f5ce95ffced25277091fdedb09446a8c2b8e
BobokulovAsilbek <147586326+BobokulovAsilbek@users.noreply.github.com>
2023-11-13 21:37:44 +0500

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -5,8 +5,8 @@ std::string problemSolution4(const std::string &macAddress) {
     // write your code here
     std::string result;
     if(macAddress[0] == 'F' && macAddress[1] == 'F') result = "Broadast";
-    else if((macAddress[1] <= '9' && (macAddress[1] - '0') % 2) || (macAddress[1] >= 'A' && (macAddress[1] - 'A') % 2 ) ) result = "Unicast";
-    else result = "Multicast";
+    else if((macAddress[1] <= '9' && (macAddress[1] - '0') % 2) || (macAddress[1] >= 'A' && (macAddress[1] - 'A') % 2 ) ) result = "Multicast";
+    else result = "Unicast";
     // make use of control flow statements
     return result;
 }
